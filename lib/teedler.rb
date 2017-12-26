a171e0329e1525b2be84eeef32cd8535d3ffc7f8
Robbie Meyer <robbie.meyer12@gmail.com>
2017-12-26 16:44:25 -0500

    Get means

--- lib/teedler.rb
+++ lib/teedler.rb
@@ -11,6 +11,9 @@ class Teedler
         totals.merge(count) { |k, a, b| a + b }
       end
 
+      n = sentences.length
+      means = word_totals.transform_values { |x| x.to_f/n }
+
     end
 
   end
