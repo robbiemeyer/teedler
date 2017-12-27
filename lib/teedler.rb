8c981bbb1c5cb2df5ab21dee1fad6967033568ec
Robbie Meyer <robbie.meyer12@gmail.com>
2017-12-26 20:49:06 -0500

    Add standard deviation

--- lib/teedler.rb
+++ lib/teedler.rb
@@ -5,15 +5,19 @@ class Teedler
 
     def summarize(text)
       sentences = Sentence.new_group text
+      n = sentences.length
       word_counts = sentences.map { |s| s.count_words }
 
-      word_totals = word_counts.reduce({}) do |totals, count|
-        totals.merge(count) { |k, a, b| a + b }
-      end
+      means = word_counts.reduce({}) do |h, count|
+        h.merge(count) { |k, a, b| a + b }
+      end.transform_values { |x| x.to_f/n }
 
-      n = sentences.length
-      means = word_totals.transform_values { |x| x.to_f/n }
-
+      sigma2s = word_counts.reduce({}) do |h, count|
+        count.each do |k, v|
+          h[k] = (h[k] || 0) + (v - means[k])**2
+        end
+        h
+      end.transform_values { |x| x/n }
     end
 
   end
