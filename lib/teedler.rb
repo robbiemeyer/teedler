bcaa5e93d594f2c14c6ca2a9041582eb85ea48a0
Robbie Meyer <robbie.meyer12@gmail.com>
2018-01-06 22:01:45 -0500

    Refactor main file

--- lib/teedler.rb
+++ lib/teedler.rb
@@ -1,31 +1,44 @@
 require 'teedler/sentence.rb'
 require 'teedler/normal_function.rb'
+require 'matrix'
+require 'pry'
 
 class Teedler
-  class << self
-
-    def summarize(text)
-      sentences = Sentence.new_group text
-      n = sentences.length
-      word_counts = sentences.map { |s| s.count_words }
-
-      means = word_counts.reduce({}) do |h, count|
-        h.merge(count) { |k, a, b| a + b }
-      end.transform_values { |x| x.to_f/n }
-
-      sigma2s = word_counts.reduce({}) do |h, count|
-        count.each do |k, v|
-          h[k] = (h[k] || 0) + (v - means[k])**2
-        end
-        h
-      end.transform_values { |x| x/n }
-
-      distribution = NormalFunction.new(means, sigma2s)
-
-      sentences.max_by.with_index do |sentence, i|
-        distribution.probability word_counts[i]
-      end.gsub(/\s?\n\s?/, '')
-    end
-
+  def initialize(text)
+    @sentences = Sentence.new_group text
+    @n = @sentences.length
+    @word_counts = @sentences.map { |s| s.count_words }
+    @words = @word_counts.map { |s| s.keys }.flatten.uniq
+    binding.pry
   end
+
+  def summarize
+    means = word_counts.reduce({}) do |h, count|
+      h.merge(count) { |k, a, b| a + b }
+    end.transform_values { |x| x.to_f/n }
+    binding.pry
+
+    sigma2s = word_counts.reduce({}) do |h, count|
+      count.each do |k, v|
+        h[k] = (h[k] || 0) + (v - means[k])**2
+      end
+      h
+    end.transform_values { |x| x/n }
+
+    distribution = NormalFunction.new(means, sigma2s)
+
+    sentences.max_by.with_index do |sentence, i|
+      distribution.probability word_counts[i]
+    end.gsub(/\s?\n\s?/, '')
+  end
+
+  private
+
+  def mean
+    word_counts.reduce({}) do |h, count|
+      h.merge(count) { |k, a, b| a + b }
+    end.transform_values { |x| x.to_f/n }
+    binding.pry
+  end
+
 end
