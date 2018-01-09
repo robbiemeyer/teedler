3f120741cd336c4a31684ebea64d178c44e9c2de
Robbie Meyer <robbie.meyer12@gmail.com>
2018-01-08 20:07:49 -0500

    Save sigma inverse as variable

--- lib/teedler/normal_function.rb
+++ lib/teedler/normal_function.rb
@@ -5,12 +5,13 @@ class NormalFunction
     @means = means
     @sigma = sigma
     @sigma_det = sigma.det
+    @sigma_inv = sigma.inverse
     @n = means.size
   end
 
   def probability(values)
     (1/ ((2*PI)**(@n/2) * @sigma_det**0.5)) * 
-      exp((-0.5 * (values - @means).covector * @sigma.inverse * (values - @means))[0])
+      exp((-0.5 * (values - @means).covector * @sigma_inv * (values - @means))[0])
   end
 
 end
